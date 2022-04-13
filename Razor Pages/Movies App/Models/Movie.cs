9e87b1a5cea087f7760a22f70a4b40d93cff2a89
Miroslav Smokov <48323537+msmokovv@users.noreply.github.com>
2022-04-13 15:16:52 +0300

    Add files via upload

---
+++ Razor Pages/Movies App/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty;
+    }
+}
